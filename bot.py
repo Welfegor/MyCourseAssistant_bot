0acc2b4bd1809cb4164b67373c8b21dbfd1f1d7b
Welfegor <46806143+Welfegor@users.noreply.github.com>
2025-02-05 10:27:03 +1000

    token updated

--- bot.py
+++ bot.py
@@ -1,35 +1,36 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 
-def start(update: Update, context: CallbackContext):
+TOKEN = "7251612310:AAEyNnRxqVRFnL8X4aBL_eK_3y3aNel8XH8"
+
+async def start(update: Update, context: CallbackContext):
     keyboard = [["📚 Поиск курсов", "🗓 Расписание"], ["🔔 Напоминания", "❓ Помощь"]]
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
-    update.message.reply_text(
+    await update.message.reply_text(
         "Здравствуйте! Я ваш помощник по курсам. Выберите действие:", reply_markup=reply_markup
     )
 
-def handle_message(update: Update, context: CallbackContext):
+async def handle_message(update: Update, context: CallbackContext):
     user_message = update.message.text.lower()
     if "поиск курсов" in user_message:
-        update.message.reply_text("Введите название курса или категорию:")
+        await update.message.reply_text("Введите название курса или категорию:")
     elif "расписание" in user_message:
-        update.message.reply_text("Расписание курсов на эту неделю: ...")
+        await update.message.reply_text("Расписание курсов на эту неделю: ...")
     elif "напоминания" in user_message:
-        update.message.reply_text("Я буду напоминать вам о предстоящих занятиях!")
+        await update.message.reply_text("Я буду напоминать вам о предстоящих занятиях!")
     elif "помощь" in user_message:
-        update.message.reply_text("Вот список часто задаваемых вопросов: ...")
+        await update.message.reply_text("Вот список часто задаваемых вопросов: ...")
     else:
-        update.message.reply_text("Извините, я вас не понял. Пожалуйста, выберите действие из меню.")
+        await update.message.reply_text("Извините, я вас не понял. Пожалуйста, выберите действие из меню.")
 
 def main():
-    updater = Updater("7251612310:AAEyNnRxqVRFnL8X4aBL_eK_3y3aNel8XH8")
+    app = Application.builder().token(TOKEN).build()
 
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(filters.text & ~filters.command, handle_message))
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
-    updater.start_polling()
-    updater.idle()
+    print("Бот запущен!")
+    app.run_polling()
 
 if __name__ == "__main__":
     main()
